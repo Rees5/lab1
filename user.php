c98834b0ccdbee5f9f918b699bd658b5f2ec41cc
Rees5 <reesalumasa@gmail.com>
2020-10-11 23:09:10 +0300

    add login functionality

--- user.php
+++ user.php
@@ -8,28 +8,39 @@
 
   class User implements Account
   {
-    protected $user_name,$user_email,$user_city,$user_image,$user_password;
+    protected $user_name;
+    protected $user_email;
+    protected $user_city;
+    protected $user_image;
+    protected $user_password;
     function __construct($user_name,$user_email,$user_city,$user_image,$user_password)
     {
-
+      $this->$user_name=$user_name;
+      $this->$user_email=$user_email;
+      $this->$user_city=$user_city;
+      $this->$user_image=$user_image;
+      $this->$user_password=$user_password;
     }
 
-    function __construct($user_name,$user_email,$user_city,$user_image,$user_password)
+    function __construct($user_email,$user_password)
     {
-
+       $this->$user_email=$user_email;
+       $this->$user_password=$user_password;
     }
-    public function register ($pdo,$user_name,$user_email,$user_city,$user_image,$user_password){
+    public function register ($pdo){
+      //hash password
+      $passwordHash = password_hash($this->$user_password,PASSWORD_DEFAULT);
       //read image attributes
-        $file_name=$user_image['name'];
-        $file_temp_location=$user_image['tmp_name'];
+        $file_name=$this->$user_image['name'];
+        $file_temp_location=$this->$user_image['tmp_name'];
         //read file extension
         $file_type=substr($file_name,strpos($file_name,'.'),strlen($file_name));
         $file_path="assets/";
         $new_name=time().$file_type;
         if(move_uploaded_file($file_temp_location,$file_path.$new_name)){
           try {
-            $stmt=$pdo->prepare("INSERT INTO company (user_name,user_email,user_city,user_image,user_password)VALUES (?,?,?,?,?)");
-            $stmt->execute([$user_name,$user_email,$user_city,$new_name,$user_password]);
+            $stmt=$pdo->prepare("INSERT INTO user_table (user_name,user_email,user_city,user_image,user_password)VALUES (?,?,?,?,?)");
+            $stmt->execute([$this->user_name,$this->user_email,$this->user_city,$new_name,$passwordHash]);
             $stmt = null;
             return "Account has been created.";
           } catch (PDOException $e) {
@@ -40,7 +51,21 @@
         }
 
     }
-    public function login($pdo,$user_name,$user_password){
+    public function login($pdo){
+      try {
+                $stmt = $pdo->prepare("SELECT user_password FROM user_table WHERE user_email=?");
+                $stmt->execute([$this->user_email]);
+                $row = $stmt->fetch();
+                if($row == null){
+                	return "Account does not exist";
+                }
+                if (password_verify($this->password,$row['user_password'])){
+                	return "Success";
+                }
+                return "Your username or password is not correct";
+            } catch (PDOException $e) {
+            	return $e->getMessage();
+            }
 
     }
     public function changePassword($pdo,$user_password,$new_user_password){
