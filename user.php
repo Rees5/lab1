2fc7b6cc080bd7bfa99ea1c99b151435226f98f9
Rees5 <reesalumasa@gmail.com>
2020-10-11 22:22:37 +0300

    update user.php

--- user.php
+++ user.php
@@ -9,17 +9,24 @@
   class User implements Account
   {
 
-    function __construct(argument)
+    function __construct()
     {
 
     }
     public function register ($pdo,$user_name,$user_email,$user_city,$user_image,$user_password){
+      try {
+      $stmt=$pdo->prepare("INSERT INTO company (user_name,user_email,user_city,user_image,user_password)VALUES (?,?,?,?,?)");
+      $stmt->execute([$user_name,$user_email,$user_city,$user_image,$user_password]);
+      $stmt = null;
+      return "Account has been created.";
+    } catch (PDOException $e) {
+      return $e->getMessage();
+    }
+    }
+    public function login($pdo,$user_name,$user_password){
 
     }
-    public function login($pdo){
-
-    }
-    public function changePassword($pdo){
+    public function changePassword($pdo,$user_password,$new_user_password){
 
     }
     public function logout ($pdo){
