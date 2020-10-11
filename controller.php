7f863045af22336f43e3fde6dbec826f1f087635
Rees5 <reesalumasa@gmail.com>
2020-10-11 22:54:16 +0300

    Add register functionality

--- controller.php
+++ controller.php
@@ -12,6 +12,7 @@
         $username = $_POST['fname'];
         $password = $_POST['password'];
         $user_city = $_POST['city'];
+        $image=$_FILES['photo'];
         $user = new User($user_name,$user_email,$user_city,$user_image,$user_password);
         echo $user->register($pdo);
     }else {
--- user.php
+++ user.php
@@ -19,14 +19,26 @@
 
     }
     public function register ($pdo,$user_name,$user_email,$user_city,$user_image,$user_password){
-      try {
-      $stmt=$pdo->prepare("INSERT INTO company (user_name,user_email,user_city,user_image,user_password)VALUES (?,?,?,?,?)");
-      $stmt->execute([$user_name,$user_email,$user_city,$user_image,$user_password]);
-      $stmt = null;
-      return "Account has been created.";
-    } catch (PDOException $e) {
-      return $e->getMessage();
-    }
+      //read image attributes
+        $file_name=$user_image['name'];
+        $file_temp_location=$user_image['tmp_name'];
+        //read file extension
+        $file_type=substr($file_name,strpos($file_name,'.'),strlen($file_name));
+        $file_path="assets/";
+        $new_name=time().$file_type;
+        if(move_uploaded_file($file_temp_location,$file_path.$new_name)){
+          try {
+            $stmt=$pdo->prepare("INSERT INTO company (user_name,user_email,user_city,user_image,user_password)VALUES (?,?,?,?,?)");
+            $stmt->execute([$user_name,$user_email,$user_city,$new_name,$user_password]);
+            $stmt = null;
+            return "Account has been created.";
+          } catch (PDOException $e) {
+            return $e->getMessage();
+          }
+        } else{
+          return "Image not uploaded. Try again"
+        }
+
     }
     public function login($pdo,$user_name,$user_password){
 
