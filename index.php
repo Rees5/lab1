24d620111183217d3ad42beb848f827a75674b38
Rees5 <reesalumasa@gmail.com>
2020-10-12 01:36:59 +0300

    Style index.php

--- index.php
+++ index.php
@@ -20,23 +20,85 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+input[type=text], input[type=password],input[type=email],input[type=file] {
+  background: linear-gradient(#1f2124,#27292c);
+  border: 1px solid #222;
+  border-radius: .3em;
+  box-shadow: 0 1px 0 rgba(255,255,255,0.1);
+  color: #FFF;
+  font-size: 13px;
+  margin-bottom: 20px;
+  padding: 8px 5px;
+  width: 80%;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
   </head>
   <body>
-    <img width="240px" height="240px" src="assets/<?php echo $_SESSION['user_image']; ?>" id="pic" /><br>
-    <hr>
-    <p id="fname">Name: <?php if(isset($_SESSION['user_name'])){ echo $_SESSION['user_name'];} ?></p>
-    <p id="email">Email: <?php if(isset($_SESSION['user_email'])){ echo $_SESSION['user_email'];} ?></p>
-    <p id="city">City: <?php if(isset($_SESSION['user_city'])){ echo $_SESSION['user_city'];} ?></p>
-    <p id="password"><?php if(isset($_SESSION['user_password'])){ echo $_SESSION['user_password'];} ?></p>
+    <h2 style="text-align:center">Profile</h2>
 
+<div class="card">
+  <img src="assets/<?php echo $_SESSION['user_image']; ?>" alt="profile" style="width:100%">
+  <h1><?php if(isset($_SESSION['user_name'])){ echo $_SESSION['user_name'];} ?></h1>
+  <p class="title"><?php if(isset($_SESSION['user_email'])){ echo $_SESSION['user_email'];} ?></p>
+  <p><?php if(isset($_SESSION['user_city'])){ echo $_SESSION['user_city'];} ?></p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <a href="#"><i class="fa fa-linkedin"></i></a>
+    <a href="#"><i class="fa fa-facebook"></i></a>
+  </div>
+  <p><a href="logout.php" onclick="confirm('Are you sure you want to logout?')"><button type="button" name="button">Sign Out</button></a></p>
+</div>
+
+  <div class="card">
+    <hr>
     <form class="">
       <input type="password" name="oldpw" id="oldpw" placeholder="Current Password">
       <input type="password" name="newpw" id="newpw" placeholder="New Password">
       <button type="button" name="button" onclick="changepw()">Change Password</button>
     </form>
-    <p><a href="login.php" onclick="confirm('Are you sure you want to logout?')"><button type="button" name="button">Logout</button></a></p>
+    </div>
    <script type="text/javascript">
   /*  var dataImage = localStorage.getItem('photo');
     var sEmail = localStorage.getItem('email');
