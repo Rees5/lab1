d73159e489389637bda4477245c57737e9f70e8f
Rees5 <reesalumasa@gmail.com>
2020-10-11 21:52:19 +0300

    Create basic methods

--- classes.php
+++ classes.php
@@ -5,4 +5,26 @@
     public function changePassword($pdo);
     public function logout ($pdo);
   }
+
+  class User implements Account
+  {
+
+    function __construct(argument)
+    {
+
+    }
+    public function register ($pdo){
+
+    }
+    public function login($pdo){
+      
+    }
+    public function changePassword($pdo){
+
+    }
+    public function logout ($pdo){
+
+    }
+  }
+
  ?>
