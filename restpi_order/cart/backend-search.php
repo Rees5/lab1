d450026151ff832a773853a823dcd26a70a35f14
Rees5 <reesalumasa@gmail.com>
2020-11-05 12:14:56 +0300

    modify search funtionality

--- restpi_order/cart/backend-search.php
+++ restpi_order/cart/backend-search.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "food_web");
+$link = mysqli_connect("localhost", "root", "", "lab1");
 
 // Check connection
 if($link === false){
@@ -30,14 +30,24 @@ if(isset($_REQUEST["term"])){
                     echo '
                     <div class="col-md-3 card" style="margin-top:12px;">
                             <div style="overflow:hidden;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); background-color:white;  padding:16px; height:400px;" align="center">
-                            	<img height="200" width="200px" src="../assets/'.$row["file_path"].'" class="img-ressponsive" /><br />
+                						<div class="flip-card">
+                							<div class="flip-card-inner">
+                								<div class="flip-card-front">
+                									<img height="200" width="100%" src="../assets/'.$row["file_path"].'" class="img-ressponsive" /><br />
+                								</div>
+                								<div class="flip-card-back">
+                									<h4 class="text-info">'.$row["food_name"].'</h4>
+                									<p class="text-info">'.$row["food_description"].'</p>
+                								</div>
+                							</div>
+                						</div><br />
                             	<h4 class="text-info">'.$row["food_name"].'</h4>
-                            	<h4 class="text-danger">Ksh. '.$row["price"] .'</h4>
-                            	<input type="text" name="quantity" id="quantity' . $row["fid"] .'" class="form-control" value="1" />
-                            	<input type="hidden" name="hidden_name" id="name'.$row["fid"].'" value="'.$row["food_name"].'" />
-                            	<input type="hidden" name="hidden_price" id="price'.$row["fid"].'" value="'.$row["price"].'" />
-                							<input type="hidden" name="hidden_description" id="price'.$row["fid"].'" value="'.$row["description"].'" />
-                            	<input type="button" name="add_to_cart" id="'.$row["fid"].'" style="margin-top:5px;" class="btn btn-success form-control add_to_cart" value="Add to Cart" />
+                            	<h4 class="text-danger">Ksh. '.$row["food_price"] .'</h4>
+                            	<input type="text" name="quantity" id="quantity' . $row["food_id"] .'" class="form-control" value="1" />
+                            	<input type="hidden" name="hidden_name" id="name'.$row["food_id"].'" value="'.$row["food_name"].'" />
+                            	<input type="hidden" name="hidden_price" id="price'.$row["food_id"].'" value="'.$row["food_price"].'" />
+                							<input type="hidden" name="hidden_description" id="price'.$row["food_id"].'" value="'.$row["food_description"].'" />
+                            	<input type="button" name="add_to_cart" id="'.$row["food_id"].'" style="margin-top:5px;" class="btn btn-success form-control add_to_cart" value="Add to Cart" />
                             </div>
                         </div>
                 		';
