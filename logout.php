43bb23bcbb6cce713a67e9b6f71b9653f65a06d4
Rees5 <reesalumasa@gmail.com>
2020-11-05 12:02:16 +0300

    fix redirect

--- logout.php
+++ logout.php
@@ -3,5 +3,13 @@ session_unset();
     session_destroy();
     session_start();
     $_SESSION['err']="Logout Successful.";
-    header("location:login.php");
+    ?>
+    <a id="link" target="_parent" href="login.php"></a>
+
+<script type="text/javascript">
+    document.getElementById('link').click();
+</script>
+    <?php
+    //echo "<script>window.top.location.href = 'login.php'; </script>";
+    //header("location:login.php");
  ?>
